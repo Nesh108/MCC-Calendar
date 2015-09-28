c933d61e48e47fe518ef4258a6ffbb2781a9cac7
Nesh108 <alberto.vaccari108@gmail.com>
2015-09-28 14:19:43 +0300

    restructured project

---
+++ server/src/deploy/config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.db = {};
+
+config.db.user_name = "mcc-calendar_root";
+config.db.password = "mcc-calendar_root_password";
+config.db.URI = "ds040898.mongolab.com:40898/mcc-calendardb";
+
+module.exports = config;
